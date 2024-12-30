6a9f7c7abf68e46e73813cd8c328b312b02e25b2
kernel6995 <don.kernel.dev@gmail.com>
2024-12-30 16:57:02 -0300

    fix navbar
    fix navbar

--- src/components/customized/NavbarPortal.tsx
+++ src/components/customized/NavbarPortal.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -16,16 +15,16 @@ import { MenuCloseIcon, MenuOpenIcon } from "@/utils/icons";
 interface NavbarPortalProps {
   defaultRoute?: string;
   listRoutes: Array<{
-    url: string,
-    label: string,
-    icon?: React.ReactNode
+    url: string;
+    label: string;
+    icon?: React.ReactNode;
   }>;
   othersRoutes?: Array<{
-    url: string,
-    label: string,
-    icon?: React.ReactNode
+    url: string;
+    label: string;
+    icon?: React.ReactNode;
   }>;
-};
+}
 
 const NavbarPortal: React.FC<NavbarPortalProps> = ({
   defaultRoute = "",
@@ -37,16 +36,19 @@ const NavbarPortal: React.FC<NavbarPortalProps> = ({
 
   const pathname = usePathname();
 
-  const handleOpenMenu = () => setOpenMenu(true);
+  const handleOpenMenu = useCallback(() => setOpenMenu(true), []);
 
-  const handleCloseMenu = () => {
+  const handleCloseMenu = useCallback(() => {
     setOpenMenu(false);
     handleCloseAccordion();
-  };
+  }, []);
 
-  const handleOpenAccordion = () => setAccordionOpen((prev) => !prev);
+  const handleOpenAccordion = useCallback(
+    () => setAccordionOpen((prev) => !prev),
+    []
+  );
 
-  const handleCloseAccordion = () => setAccordionOpen(false);
+  const handleCloseAccordion = useCallback(() => setAccordionOpen(false), []);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -58,15 +60,21 @@ const NavbarPortal: React.FC<NavbarPortalProps> = ({
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [openMenu, handleCloseMenu]);
+  }, [openMenu]);
 
   const { tones } = useThemeController();
 
   const menuContent = (
     <div className="bg-stone-950/90 h-screen w-screen fixed top-0 left-0 z-30">
-      <nav className={`bg-stone-300 dark:bg-stone-900 h-screen w-4/5 sm:w-2/4 md:w-1/3 lg:w-1/4 ${tones.borderColor.normal} border-l-4 border-y-4 rounded-l-xl absolute top-0 right-0 space-y-2 flex flex-col md:px-2 transition-all`}>
-        <header className={`${tones.borderColor.normal} border-b-4 flex justify-between items-center py-1 px-2`}>
-          <h2 className="text-stone-900 dark:text-stone-200 text-xl uppercase font-bold">menú</h2>
+      <nav
+        className={`bg-stone-300 dark:bg-stone-900 h-screen w-4/5 sm:w-2/4 md:w-1/3 lg:w-1/4 ${tones.borderColor.normal} border-l-4 border-y-4 rounded-l-xl absolute top-0 right-0 space-y-2 flex flex-col md:px-2 transition-all`}
+      >
+        <header
+          className={`${tones.borderColor.normal} border-b-4 flex justify-between items-center py-1 px-2`}
+        >
+          <h2 className="text-stone-900 dark:text-stone-200 text-xl uppercase font-bold">
+            menú
+          </h2>
 
           <Button
             title={"cerrar menú (Esc)"}
@@ -85,9 +93,11 @@ const NavbarPortal: React.FC<NavbarPortalProps> = ({
                   onClick={handleCloseMenu}
                   className={`
                     text-xl py-1 px-2 flex items-center space-x-2
-                    ${pathname === "/" + route.url ?
-                      `${tones.bgColor.normal} text-stone-200` :
-                      "hover:bg-stone-400 hover:dark:bg-stone-700"}
+                    ${
+                      pathname === "/" + route.url
+                        ? `${tones.bgColor.normal} text-stone-200`
+                        : "hover:bg-stone-400 hover:dark:bg-stone-700"
+                    }
                   `}
                 >
                   {route.icon} {route.label}
@@ -103,7 +113,9 @@ const NavbarPortal: React.FC<NavbarPortalProps> = ({
               className={`${tones.borderColor.normal} border-y-4 flex justify-between items-center py-1 pl-2 pr-4 md:pr-2 cursor-pointer`}
               onClick={handleOpenAccordion}
             >
-              <h3 className="text-stone-900 dark:text-stone-200 text-xl uppercase font-bold">otras rutas</h3>
+              <h3 className="text-stone-900 dark:text-stone-200 text-xl uppercase font-bold">
+                otras rutas
+              </h3>
 
               <span>{accordionOpen ? "▲" : "▼"}</span>
             </header>
@@ -117,9 +129,11 @@ const NavbarPortal: React.FC<NavbarPortalProps> = ({
                       onClick={handleCloseMenu}
                       className={`
                       text-xl py-1 px-2 flex items-center space-x-2
-                      ${pathname === "/" + route.url ?
-                          `${tones.bgColor.normal} text-stone-200` :
-                          "hover:bg-stone-400 hover:dark:bg-stone-700"}
+                      ${
+                        pathname === "/" + route.url
+                          ? `${tones.bgColor.normal} text-stone-200`
+                          : "hover:bg-stone-400 hover:dark:bg-stone-700"
+                      }
                     `}
                     >
                       {route.icon} {route.label}
@@ -146,7 +160,8 @@ const NavbarPortal: React.FC<NavbarPortalProps> = ({
         styles="fixed top-2 right-2"
       />
 
-      {openMenu && createPortal(menuContent, document.getElementById("navbar-portal-root")!)}
+      {openMenu &&
+        createPortal(menuContent, document.getElementById("navbar-portal-root")!)}
     </>
   );
 };
